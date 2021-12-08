c6486f3ae4d2e72bf239abf6f36c0e2fa3b3b753
josegonzah <jose.gonzalezh@udea.edu.co>
2021-12-07 19:16:55 -0500

    calculadora funcional simple

---
+++ calculadoraFuncional.js
@@ -0,0 +1,45 @@
+const operadores = ["+", "*", "-", "/"];
+//isOperator checks if a given character is an operator
+const isOperador = char => operadores.includes(char);
+const operacionesBasicas = {
+	"/": (a, b) => a / b,
+	"*": (a, b) => a * b,
+	"+": (a, b) => a + b,
+	"-": (a, b) => a - b,
+}
+
+const juntarOperaciones = (resultadoAcumulado, i) => {
+	if (i === -1) return resultadoAcumulado;
+	const resultado = operacionesBasicas[resultadoAcumulado[i]](+resultadoAcumulado[i-1], +resultadoAcumulado[i+1]);
+	return resultadoAcumulado.map((item, j) => i - 1 === j ? resultado : item)
+	.filter((item, j) => j < i || j > i + 1);
+}
+
+const siguienteOperador = resultadoAcumulado => {
+	const nextIndex = resultadoAcumulado.findIndex(operator => ["*", "/"].includes(operator));
+	return nextIndex !== -1 ? 
+	nextIndex : 
+	resultadoAcumulado.findIndex(operator => ["+", "-"].includes(operator));
+}
+
+const calcular = (resultadoAcumulado) => {
+	if (resultadoAcumulado.length === 1) return resultadoAcumulado[0];
+	return calcular(juntarOperaciones(resultadoAcumulado, siguienteOperador(resultadoAcumulado)))
+}
+
+
+const formatearOperaciones = (stringOperaciones, arr = "") => {
+	if (!stringOperaciones) return arr.split(" ");
+	return formatearOperaciones(
+		stringOperaciones.slice(1),
+		arr.concat(isOperador(firstChar) ? ` ${stringOperaciones[0]} ` : stringOperaciones[0])
+	)
+}
+
+/* takes a calculation string and returns a result */
+const calculate = calculationString => {
+	const calculation = formatearOperaciones(calculationString)
+	return calcular(calculation, siguienteOperador(calculation));
+}
+
+console.log(calculate("13+6*2"));
